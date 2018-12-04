c4cb7e38429e7b9873f467c2f9dc61d99451b7ee
Kansei Kogami <kk7221dev@gmail.com>
2018-12-04 16:03:35 +0900

    add: todo

--- app.rb
+++ app.rb
@@ -1,27 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'dotenv'
-require 'selenium-webdriver'
+require_relative './lib/auto_tweet'
 
 get '/' do
   erb :index
 end
 
 get '/tweet' do
-  Dotenv.load
+  # todo: 絵文字の追加, 非同期処理
+  messages = %w(うんこなう うんぴょなう うんちっちなう うんちょなう うんぴょこなう うんぽこなう unko\ now I'm\ pooping\ now 大便大出來了 똥이\ 마렵따 )
 
-  driver = Selenium::WebDriver.for :firefox
-
-  driver.navigate.to 'https://twitter.com/login?lang=ja'
-
-  email = driver.find_element(:name, 'session[username_or_email]')
-  email.send_keys ENV.fetch('EMAIL')
-
-  email = driver.find_element(:name, 'session[password]')
-  email.send_keys ENV.fetch('PASSWORD')
-
-  button = driver.find_element(:css, '.submit')
-  button.click
-
+  auto_tweet(messages.sample)
   redirect '/'
 end
