7c137283e682f952793e5713de346f7383653c21
Kansei Kogami <kk7221dev@gmail.com>
2018-12-05 11:03:52 +0900

    fix for omniauth-twitter

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'omniauth'
+require 'omniauth-twitter'
+
 require_relative './lib/auto_tweet'
+
+use Rack::Session::Cookie
+
+use OmniAuth::Builder do
+  provider :twitter, ENV.fetch('CONSUMER_KEY'), ENV.fetch('CONSUMER_SECRET_KEY')
+end
 
 get '/' do
   erb :index
+end
+
+post '/auth/open_id' do
+  'Hello World'
+end
+
+post '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
 end
 
 get '/tweet' do
