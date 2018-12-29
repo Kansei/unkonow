53162d5203e36fa4ccc6ec84c9dccc0ac406d7b2
Kansei Kogami <kk7221dev@gmail.com>
2018-12-29 19:59:54 +0900

    don't support pc

--- app.rb
+++ app.rb
@@ -1,13 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv'
+require 'rack/user_agent'
 
 require_relative './lib/outh_twitter'
 
-enable :sessions
-set :request_token, ""
+configure do
+  use Rack::UserAgent
+  enable :sessions
+  set :request_token, ""
+end
 
 get '/' do
+  redirect '/not_support' if request.from_pc?
+
   if session[:access_token].nil?
     settings.request_token = twitter.get_request_token
     redirect settings.request_token.authorize_url
@@ -23,6 +29,10 @@ get '/authorize' do
   session[:access_token] = access_token.token
   session[:access_key] = access_token.secret
   redirect '/'
+end
+
+get '/not_support' do
+  erb :not_support
 end
 
 get '/tweet' do
