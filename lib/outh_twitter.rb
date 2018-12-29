0a9d152eff27fb75059ec6789254f9de36a58d42
Kansei Kogami <kk7221dev@gmail.com>
2018-12-29 15:42:23 +0900

    add: oauth_callback to fix Unauthorized 401

--- lib/outh_twitter.rb
+++ lib/outh_twitter.rb
@@ -22,8 +22,9 @@ class OuthTwitter
   def get_access_token(pin)
     request_token = @consumer.get_request_token
     access_token = request_token.get_access_token(
-       :oauth_token => request_token.token,
-       :oauth_verifier => pin
+       oauth_token: request_token.token,
+       oauth_verifier: pin,
+       oauth_callback: 'https://unkonow.herokuapp.com/authorize'
     )
   end
 end
