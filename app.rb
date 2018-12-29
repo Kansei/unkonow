06c8ecc635a3f3b669d33da5050a7ffae4fd343e
Kansei Kogami <kk7221dev@gmail.com>
2018-12-29 18:06:25 +0900

    done: outh twitter and tweet

--- app.rb
+++ app.rb
@@ -1,38 +1,44 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv'
-require 'oauth'
 
 require_relative './lib/outh_twitter'
 
 enable :sessions
+set :request_token, ""
 
 get '/' do
-  if session[:access_token].nil? || session[:access_key].nil?
-    redirect outh_twitter.get_authorize_url
+  if session[:access_token].nil?
+    settings.request_token = twitter.get_request_token
+    redirect settings.request_token.authorize_url
   end
   erb :index
 end
 
-get '/tweet' do
-  erb :index
-end
-
-# get '/authorize' do
-#   erb :authorize
-# end
-
 get '/authorize' do
   oauth_verifier = params[:oauth_verifier]
-  access_token = outh_twitter.get_access_token(oauth_verifier)
+  request_token = settings.request_token
+  access_token = twitter.get_token_and_key_for_access(request_token, oauth_verifier)
+
   session[:access_token] = access_token.token
   session[:access_key] = access_token.secret
-  erb :index
+  redirect '/'
+end
+
+get '/tweet' do
+  access_token = twitter.get_access_token(session[:access_token], session[:access_key])
+
+  messages = ["こんにちは", "おはよう", "こんばんわ"]
+  result = twitter.tweet(messages.sample, access_token)
+
+  puts result
+
+  redirect '/'
 end
 
 helpers do
-  def outh_twitter
-    @outh_twitter ||= OuthTwitter.new
+  def twitter
+    @twitter ||= OuthTwitter.new
   end
 end
 
--- lib/outh_twitter.rb
+++ lib/outh_twitter.rb
@@ -1,4 +1,5 @@
 require 'oauth'
+require 'json'
 require 'dotenv'
 
 class OuthTwitter
@@ -13,18 +14,24 @@ class OuthTwitter
     )
   end
 
-  def get_authorize_url
-    request_token = @consumer.get_request_token(oauth_callback: 'https://unkonow.herokuapp.com/authorize')
-
-    request_token.authorize_url
+  def get_request_token
+    @consumer.get_request_token(oauth_callback: 'https://unkonow.herokuapp.com/authorize')
   end
 
-  def get_access_token(oauth_verifier)
+  def get_token_and_key_for_access(request_token, oauth_verifier)
     @consumer.options.delete(:oauth_callback)
-    request_token = @consumer.get_request_token
-    access_token = request_token.get_access_token(
+    request_token.get_access_token(
        oauth_token: request_token.token,
        oauth_verifier: oauth_verifier
     )
+  end
+
+  def get_access_token(access_token, access_key)
+    OAuth::AccessToken.new(@consumer, access_token, access_key)
+  end
+
+  def tweet(message, access_token)
+    response = access_token.post('https://api.twitter.com/1.1/statuses/update.json', status: message)
+    response.code
   end
 end
