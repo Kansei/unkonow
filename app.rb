c28897c6a2ba7cfc8197f19ce23070cfad3051c6
Kansei Kogami <kk7221dev@gmail.com>
2018-12-29 15:40:47 +0900

    add helper, gem oauth

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'dotenv'
+require 'oauth'
 
 require_relative './lib/outh_twitter'
 
 enable :sessions
 
 get '/' do
-  if session.nil?
-    @twitter = OuthTwitter.new
-    redirect @twitter.get_authorize_url
+  if session[:access_token].nil? || session[:access_key].nil?
+    redirect outh_twitter.get_authorize_url
   end
   erb :index
 end
@@ -24,11 +24,15 @@ end
 
 post '/authorize' do
   pin = params[:pin]
-  access_token = @twitter.get_access_token(pin)
+  access_token = outh_twitter.get_access_token(pin)
   session[:access_token] = access_token.token
   session[:access_key] = access_token.secret
   erb :index
 end
 
-
+helpers do
+  def outh_twitter
+    @outh_twitter ||= OuthTwitter.new
+  end
+end
 
